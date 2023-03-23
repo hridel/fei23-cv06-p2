4bd2a1d5c8d3b7aeac4769a88ec4ec5c1e2a91e9
Jan Hridel <hridel@gmail.com>
2023-03-23 09:24:55 +0100

    init

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int **matrix;  // declare pointer to pointer to integer
+    int n, i, j;
+
+    // prompt user for matrix size
+    printf("Zadejte n určující velikost matice (n x n): ");
+    scanf("%d", &n);
+
+    // dynamically allocate memory for matrix
+    matrix = (int**) malloc(n * sizeof(int*));
+    for (i = 0; i < n; i++) {
+        matrix[i] = (int*) malloc(n * sizeof(int));
+    }
+
+    // check if allocation was successful
+    if (matrix == NULL) {
+        printf("Error: Nelze alokovat pamet.\n");
+        return 1;
+    }
+
+    // prompt user to enter values for matrix
+    printf("Zadejte hodnoty pro matici %d*%d:\n", n, n);
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // print out the values in the matrix
+    printf("Hodnoty matice jsou:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    // free dynamically allocated memory
+    for (i = 0; i < n; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Příklad 2
+## Dynamické dvojrozměrné pole
+
+Napište program, který dynamicky alokuje dvojrozměrné pole celých **N x N** čísel.
+
+Pole načtěte a následně vytiskněte.
+
+---
+
+_Nápověda:_
+
+Pro alokování dvojrozměrného pole (matice), je potřeba použít dvě úrovně ukazatelů, tedy ukazatel na ukazatel celých čísel.
+
+`int **matrix;  // declare pointer to pointer to integer`
